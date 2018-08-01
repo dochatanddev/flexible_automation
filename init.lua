4f00a004b768dd9d88d3d9d859fdc5716d808268
UserName <UserEmail@NotAnEmail.rly>
2018-08-02 02:15:02 +1000

    implemented taking references of block positions

--- init.lua
+++ init.lua
@@ -1,1 +1,48 @@
-print("This file will be run at lssssoad time!")
+flexible_automation = {
+  ref = {
+    itemId = 'flexible_automation:ref',
+    itemDefinition = {
+      description = 'Reference Node for %s',
+      stack_max = 1,
+    },
+    register = function()
+      minetest.register_craftitem(flexible_automation.ref.itemId, flexible_automation.ref.itemDefinition)
+    end,
+  },
+  refTaker = {
+    itemId = 'flexible_automation:reftaker',
+    itemDefinition = {
+      description = 'Reference Taker Tool',
+      on_use = function(itemstack, placer, pointed_thing)
+        local pos = pointed_thing.under
+        local player_name = user and user:get_player_name() or ""
+
+        if minetest.is_protected(pos, player_name) then
+          minetest.record_protection_violation(pos, player_name)
+          return
+        end
+  
+        if pointed_thing.type == 'node' then
+          flexible_automation.refTaker.takeRef(itemstack, placer, pos)
+        end
+      end,
+    },
+    takeRef = function(itemstack, placer, pos)
+      local ref = ItemStack(flexible_automation.ref.itemId)
+      local meta = ref:get_meta()
+      local desc = flexible_automation.ref.itemDefinition.description
+      local posString = minetest.pos_to_string(pos)
+      meta:set_string('description', string.format(desc, posString)) 
+      meta:set_string('stored_pos', posString)
+      
+      local playerInventory = placer:get_inventory()
+      playerInventory:add_item('main', ref)
+    end,
+    register = function()
+      minetest.register_tool(flexible_automation.refTaker.itemId, flexible_automation.refTaker.itemDefinition)
+    end,
+  },
+}
+
+flexible_automation.ref.register()
+flexible_automation.refTaker.register()
--- readme.md
+++ readme.md
@@ -3,16 +3,15 @@
 ## What the mod does:
 Mod adds 3 things:
 1. refTaker tool (id `flexible_automation:reftaker`)
-1. refNode (id `flexible_automation:refnode`)
+1. ref (id `flexible_automation:ref`)
 1. filter (id `flexible_automation:filter`)
 
 ## How to use these things?
-Use refTaker to take refNode (basically a link to that block
+Use refTaker to take ref (basically a link to that block
 in a form of new block).
 Example: use refTaker on 2 different chests. Place 
-refNodes of these chests somewhere and use filter
-to transfer items between refNodes. See that by
-doing that you do moving items between chests.
+refs of these chests in filter
+to transfer items between referenced chests.
 
 ## How to install this mod:
 You have 2 options:
